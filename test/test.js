88295f57bd840783a1d53d0fd04334160fb5ca34
wodpachua <molarag21996@gmail.com>
2024-07-07 11:26:13 +0300

    Test Last TDD Cycle 2

--- test/test.js
+++ test/test.js
@@ -5,4 +5,9 @@ describe("Finding factorial of number", function () {
   it("Correct factorial should be returned", function () {
     assert.equal(factorial(5), 120);
   });
+
+  it("Factorial of '0' and '1' should return '1'", function () {
+    assert.equal(factorial(1), 1);
+    assert.equal(factorial(0), 1);
+  });
 });
